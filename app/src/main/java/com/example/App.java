ed0c1db70b8cc133c20166c4d12d8545a0c28ee9
vivek <vivek.bagde1203@gmail.com>
2025-12-23 22:28:44 +0530

    added

---
+++ app/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        port(8080);
+
+        get("/", (req, res) -> "Student Management App - VERSION v1 (BLUE)");
+    }
+}
+
